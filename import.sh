eef817f5b912f4c34868d1668e40b60b50412c73
Christian Mayer <christian@fox21.at>
2015-01-31 11:47:57 +0100

    Clean up.

--- import.sh
+++ import.sh
@@ -28,7 +28,6 @@ done
 SUBLIME_USER_DIR="$HOME/Library/Application Support/Sublime Text 2/Packages/User"
 if [[ -d $SUBLIME_USER_DIR ]]; then
 	echo 'import sublimetext user files'
-	#echo $SUBLIME_USER_DIR
 	
 	copytmp 'sublimetext/keymaps/*.sublime-keymap' "$SUBLIME_USER_DIR"
 	copytmp 'sublimetext/settings/*.sublime-settings' "$SUBLIME_USER_DIR"
@@ -37,7 +36,6 @@ fi
 SUBLIME_SNIPPET_PHP_DIR="$HOME/Library/Application Support/Sublime Text 2/Packages/PHP"
 if [[ -d $SUBLIME_SNIPPET_PHP_DIR ]]; then
 	echo 'import sublimetext PHP snippet files'
-	#echo $SUBLIME_SNIPPET_PHP_DIR
 	
 	copytmp 'sublimetext/snippets/PHP/*.sublime-snippet' "$SUBLIME_SNIPPET_PHP_DIR" || echo failed
 	copytmp 'sublimetext/snippets/PHP/*.sublime-completions' "$SUBLIME_SNIPPET_PHP_DIR" || echo failed
@@ -46,7 +44,6 @@ fi
 SUBLIME_SNIPPET_PERL_DIR="$HOME/Library/Application Support/Sublime Text 2/Packages/Perl"
 if [[ -d $SUBLIME_SNIPPET_PERL_DIR ]]; then
 	echo 'import sublimetext Perl snippet files'
-	#echo $SUBLIME_SNIPPET_PERL_DIR
 	
 	copytmp 'sublimetext/snippets/Perl/*.sublime-snippet' "$SUBLIME_SNIPPET_PERL_DIR" || echo failed
 	#copytmp 'sublimetext/snippets/Perl/*.sublime-completions' "$SUBLIME_SNIPPET_PERL_DIR" || echo failed
@@ -55,7 +52,6 @@ fi
 SUBLIME_SNIPPET_CPP_DIR="$HOME/Library/Application Support/Sublime Text 2/Packages/C++"
 if [[ -d $SUBLIME_SNIPPET_CPP_DIR ]]; then
 	echo 'import sublimetext C++ snippet files'
-	#echo $SUBLIME_SNIPPET_CPP_DIR
 	
 	copytmp 'sublimetext/snippets/C++/*.sublime-snippet' "$SUBLIME_SNIPPET_CPP_DIR" || echo failed
 fi
