237a37eefded627cc25da6311c4bb880dbc4b994
Christian Mayer <christian@fox21.at>
2015-06-07 22:27:40 +0200

    Set bash for root shell.
    Bash is the best shell.

--- osx.sh
+++ osx.sh
@@ -8,6 +8,8 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until `.osx` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
+echo root
+sudo chsh -s /bin/bash
 
 echo screenshot
 # Screenshot: Change the location
