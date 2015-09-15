687c784d1884ff36a99978dfafa1800642650d64
Christian Mayer <christian@fox21.at>
2015-09-15 15:26:02 +0200

    TeX Uninstall script added. #RN

---
+++ shell-macosx/bin/tex-uninstall.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# https://www.tug.org/mactex/uninstalling.html
+
+
+RM="rm -vrfd"
+LS="ls -lad"
+DIRS="/Applications/TeX.app /Library/TeX /Library/PreferencePanes/TeXDistPrefPane.prefPane /usr/local/texlive /usr/texbin"
+BIN_FILES="/etc/paths.d/TeX"
+MAN_FILES_DE=""
+MAN_FILES_C=""
+
+# Ask for the administrator password upfront.
+echo 'ask for sudo'
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until script has finished.
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+for dir in $DIRS; do
+	( $LS $dir 2> /dev/null; [ -d $dir ] && echo sudo $RM $dir )
+done
+
+for file in $BIN_FILES $MAN_FILES_DE $MAN_FILES_C; do
+	( $LS $file 2> /dev/null; [ -f $file ] && echo sudo $RM $file )
+done
+
+echo 'done'
