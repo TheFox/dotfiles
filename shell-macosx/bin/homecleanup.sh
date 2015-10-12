ba40f1dbfad705a82b23c24106d0db3411979120
Christian Mayer <christian@fox21.at>
2015-10-12 13:03:58 +0200

    Home clean up script added. #RN

---
+++ shell-macosx/bin/homecleanup.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+RM="rm -frd"
+
+cd ~
+
+pwd
+echo
+
+set -x
+
+$RM .android
+$RM .atom
+$RM .bundle
+$RM .cardpeek
+$RM .cardpeek.log
+$RM .cocoapods
+$RM .gource
+$RM .gradle
+$RM .gtk-bookmarks
+$RM .mediatomb
+$RM .mitmproxy
+$RM .mysql_history
+$RM .pdf-over
+$RM .python-eggs
+$RM .subversion
+$RM .wakatime.*
+$RM .WebIde80
