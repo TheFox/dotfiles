ee6956c4c127a556dad1282efa328f89ff249dae
Christian Mayer <christian@fox21.at>
2015-11-02 21:04:33 +0100

    iPhone internet sharing scripts added. #RN

---
+++ shell-macosx/bin/route-normal.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Ask for the administrator password upfront.
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until script has finished.
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+echo
+
+networksetup -getinfo 'Ethernet'
+echo
+
+networksetup -getinfo 'iPhone USB'
+echo
+
+networksetup -getinfo 'Wi-Fi'
+echo
+
+sudo ifconfig en0
+echo
+
+sudo ifconfig en1
+echo
+
+sudo ifconfig en5
+echo
+
+
+echo
+echo 'press any-key do continue, ctrl-c to abort.'
+read
+
+echo 'up en0'
+sudo ifconfig en0 up
+echo "status: $?"
+echo
+
+echo 'up en1'
+sudo ifconfig en1 up
+echo "status: $?"
+echo
+
+echo 'down en5'
+sudo ifconfig en5 down
+echo "status: $?"
+echo
