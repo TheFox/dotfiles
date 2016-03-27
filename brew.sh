cbb28f80779931a05cf7f5014d6690d492690ee5
Christian Mayer <christian@fox21.at>
2016-03-27 17:35:07 +0200

    VLC placeholder added. #RN

--- brew.sh
+++ brew.sh
@@ -80,6 +80,7 @@ brew install Caskroom/cask/keepassx
 brew install Caskroom/cask/sequel-pro
 #brew install Caskroom/cask/skype
 brew install Caskroom/cask/thunderbird
+#brew install Caskroom/cask/vlc
 
 
 brew cleanup
