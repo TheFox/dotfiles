29b60465d9f25da3f2021db4c156c9526e19aace
Christian Mayer <christian@fox21.at>
2016-03-27 15:14:34 +0200

    Delete old files. #RN

--- import.sh
+++ import.sh
@@ -13,5 +13,11 @@ if [ "$un" = "Darwin" ]; then
 	script_name="macosx"
 fi
 
+echo 'remove old files'
+rm -f ~/bin/changelog.sh
+rm -f ~/bin/functions.sh
+rm -f ~/bin/functions-inc.sh
+rm -f ~/bin/release
+
 echo "script_name: '${script_name}'"
 ./import_${script_name}.sh
