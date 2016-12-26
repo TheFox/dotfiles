bb4ed293ca5c93a4000228e96cf46459ae303e9e
Christian Mayer <christian@fox21.at>
2016-12-26 23:33:03 +0100

    sudo remove for Jekyll reinstallation.

--- shell/common/bin/jekyll_reinstall.sh
+++ shell/common/bin/jekyll_reinstall.sh
@@ -10,23 +10,23 @@ set -e
 function list(){
 	for gem_name in github-pages-health-check jekyll jekyll-coffeescript jekyll-feed jekyll-gist jekyll-github-metadata jekyll-mentions jekyll-paginate jekyll-redirect-from jekyll-sass-converter jekyll-seo-tag jekyll-sitemap jemoji kramdown liquid listen mercenary rouge terminal-table; do
 		echo $gem_name
-		sudo gem list --local $gem_name || true
-		#sudo gem uninstall $gem_name -a -x -I
+		gem list --local $gem_name || true
+		#gem uninstall $gem_name -a -x -I
 	done
 }
 
 #list; exit
 
-sudo gem list --local github-pages || true
+gem list --local github-pages || true
 echo '------'
 echo
 
 echo 'INSTALL'
-sudo gem uninstall github-pages -a -x -I
-sudo gem install github-pages -v 78
-sudo gem install redcarpet -v 3.3.3
+gem uninstall github-pages -a -x -I
+gem install github-pages -v 78
+gem install redcarpet -v 3.3.3
 
 echo '------'
 echo
 list
-sudo gem list --local github-pages
+gem list --local github-pages
