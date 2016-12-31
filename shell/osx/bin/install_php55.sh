b30153bf12a87eebb9882986e2657e4de17fc2dc
Christian Mayer <christian@fox21.at>
2016-12-31 17:48:24 +0100

    PHP install scripts added. #RN
    Ref: https://gist.github.com/TheFox/8924850

---
+++ shell/osx/bin/install_php55.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+brew install php55 --with-thread-safety --without-snmp --with-imap
+#brew install php55-pthreads --without-snmp
+brew install php55-mcrypt
+brew install php55-xdebug
---
+++ shell/osx/bin/install_php56.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+brew install php56 --with-thread-safety --without-snmp --with-imap --with-httpd24
+brew install mcrypt --build-from-source php56-mcrypt --build-from-source
+brew install php56-xdebug
