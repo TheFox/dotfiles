82c43e90adf41f7dc347b7177ab8c9992e916b97
Christian Mayer <christian@fox21.at>
2017-01-26 17:20:08 +0100

    Install xdebug from source.
    Ref: https://github.com/Homebrew/homebrew-php/issues/1556

--- shell/osx/bin/install_php56.sh
+++ shell/osx/bin/install_php56.sh
@@ -2,4 +2,4 @@
 
 brew install php56 --with-thread-safety --without-snmp --with-imap --with-httpd24
 brew install mcrypt --build-from-source php56-mcrypt --build-from-source
-brew install php56-xdebug
+brew install php56-xdebug --build-from-source
