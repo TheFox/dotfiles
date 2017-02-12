a873e621ff513e89548c1573aff16cd8df6b2177
Christian Mayer <christian@fox21.at>
2017-02-12 15:13:18 +0100

    Remove a file from Git History script moved from GitHubGist.
    https://gist.github.com/TheFox/62f73dcfbb0ba6593544

---
+++ shell/common/bin/git_rm_file.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Remove a file from Git History
+
+# http://dound.com/2009/04/git-forever-remove-files-or-folders-from-history/
+# http://stackoverflow.com/a/14728706
+# https://gist.github.com/TheFox/62f73dcfbb0ba6593544
+
+files="$@"
+
+git remote -v
+git remote rm origin
+
+git filter-branch --index-filter "git rm -rf --cached --ignore-unmatch $files" HEAD
+
+rm -vrf .git/refs/original/ && git reflog expire --all && git gc --aggressive --prune
+
+#git remote add origin <URL>
+#git push -f origin master
